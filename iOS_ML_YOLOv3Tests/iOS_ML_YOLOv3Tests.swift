64400a8fc46405cda2f6850a9e5478a762890658
roanacla <roanacla@me.com>
2020-11-01 12:59:09 -0800

    First commit

---
+++ iOS_ML_YOLOv3Tests/iOS_ML_YOLOv3Tests.swift
@@ -0,0 +1,33 @@
+//
+//  iOS_ML_YOLOv3Tests.swift
+//  iOS_ML_YOLOv3Tests
+//
+//  Created by Roger Navarro on 11/1/20.
+//
+
+import XCTest
+@testable import iOS_ML_YOLOv3
+
+class iOS_ML_YOLOv3Tests: XCTestCase {
+
+    override func setUpWithError() throws {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDownWithError() throws {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() throws {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() throws {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
